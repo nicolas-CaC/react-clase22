98726473c7e383074386033bfcf1dda6403da360
nicolas-CaC <nicolas.machicado@bue.edu.ar>
2022-11-30 21:39:21 -0300

    Servidor Express Node - API

--- server.js
+++ server.js
@@ -1,31 +1,3 @@
-import express from 'express';
+import { Server } from "./src/Server/Server.js";
 
-const app = express();
-
-app.get('/', (req, res) => {
-    res.json({ saludo: 'cómo le va? Desde el Get' })
-})
-app.post('/', (req, res) => {
-    res.json({ saludo: 'cómo le va? Desde el Post' })
-})
-app.put('/', (req, res) => {
-    res.json({ saludo: 'cómo le va? Desde el Put' })
-})
-app.delete('/', (req, res) => {
-    res.json({ saludo: 'cómo le va? Desde el Delete' })
-})
-app.patch('/', (req, res) => {
-    res.json({ saludo: 'cómo le va? Desde el Patch' })
-})
-app.options('/', (req, res) => {
-    res.json({ saludo: 'cómo le va? Desde el Options' })
-})
-app.purge('/', (req, res) => {
-    res.json({ saludo: 'cómo le va? Desde el Purge' })
-})
-
-const PORT = 8080;
-
-const server = app.listen(PORT, () =>
-    console.log(`Server listening at ${server.address().port}
-    - http://localhost:${PORT}`));
+Server.getInstance(8080).run()
